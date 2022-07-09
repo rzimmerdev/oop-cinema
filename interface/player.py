28684a583a82b0dfbc82de65d687d62b9ac1887a
Rafael Zimmer <rzimmerdev@gmail.com>
2022-07-09 02:39:09 -0300

    Created video player and interface manager

---
+++ interface/player.py
@@ -0,0 +1,80 @@
+import os
+import tkinter as tk
+from enum import Enum
+
+import cv2 as cv2
+from PIL import Image, ImageTk
+
+
+class Position(Enum):
+    START = (0.0, 0.0)
+    TOP = (0.0, 0.5)
+    CENTER = (0.5, 0.5)
+    BOTTOM = (1, 0.5)
+    LEFT = (0.5, 0)
+    RIGHT = (0.5, 0.5)
+
+
+class VideoPlayer:
+    def __init__(self, window_root, window_canvas, path,
+                 size: tuple = (640, 360), pos: Position = Position.TOP, fps: int = 15):
+        self.root = window_root
+        self.canvas = window_canvas
+        self.path = path
+
+        self.pos = pos
+        self.size = size
+        self.fps = fps
+        self._videobuffer = None
+        self._frame = None
+
+    def is_valid(self):
+        return self._videobuffer.isOpened()
+
+    def __get_shape(self):
+        return self._videobuffer.get(cv2.CAP_PROP_FRAME_WIDTH), self._videobuffer.get(cv2.CAP_PROP_FRAME_HEIGHT)
+
+    def open(self, file):
+        url = self.path + file
+        self._videobuffer = cv2.VideoCapture(url)
+
+        if not self.is_valid():
+            raise ValueError("Invalid URL for video file.", self._videobuffer)
+
+        return self.__get_shape()
+
+    def get_frame(self):
+        if self.is_valid():
+            is_loaded, frame = self._videobuffer.read()
+            if is_loaded:
+                frame = cv2.resize(frame, self.size)
+                return cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        return None
+
+    def play(self):
+        """Recursive method for loading sequence of frames from Video buffer, displaying them to the
+        root window, within the canvas element. Calls end when buffer is empty.
+
+        Examples:
+            >>> movie = VideoPlayer("../movies/")
+            >>> movie.open("Planet_of_the_Apes.mp4")
+            >>> movie.play()
+        """
+        # Read next frame from video source buffer
+        frame = self.get_frame()
+        # Test if next frame could be read, else quit video player
+
+        offset_y = (self.canvas.winfo_width() - self.size[0]) * self.pos.value[0]
+        offset_x = (self.canvas.winfo_height() - self.size[1]) * self.pos.value[1]
+
+        if frame is not None:
+            self._frame = ImageTk.PhotoImage(image=Image.fromarray(frame))
+            self.canvas.create_image(offset_x, offset_y, image=self._frame, anchor=tk.NW)
+        else:
+            return
+
+        # Recursive call to play, displaying next frame from video buffer
+        self.root.after(self.fps, self.play)
+
+    def __del__(self):
+        self._videobuffer.release() if self.is_valid() else None
--- interface/view.py
+++ interface/view.py
@@ -4,29 +4,35 @@
 # Should only be called by parent methods, as no functionality is managed herein.
 #
 import tkinter as tk
-from tkinter import ttk
-import cv2
+
+from player import VideoPlayer, Position
 
 
 class ViewFactory:
     def __init__(self, franchise: str = None):
         self.franchise = franchise
-        self.video = None
         self.root = tk.Tk()
-        content = tk.Label(text="Hello World!")
-        content.pack()
 
-    def show(self):
+    def show(self, canvas):
+        canvas.pack()
+        self.root.title(self.franchise)
         self.root.mainloop()
 
-    def play_video(self, url):
-        self.video = cv2.VideoCapture(url)
-        pass
+    def play_video(self, file, src_path):
+        canvas = tk.Canvas(self.root, width=1920, height=1080)
+        canvas.pack()
+
+        movie_player = VideoPlayer(self.root, canvas, src_path, fps=15)
+        movie_player.open(file)
+
+        movie_player.play()
+        return canvas
 
 
 def main():
     cinema1 = ViewFactory(franchise="CinePlex")
-    cinema1.show()
+    video = cinema1.play_video("water.mp4", "../films/")
+    cinema1.show(video)
 
 
 if __name__ == "__main__":
