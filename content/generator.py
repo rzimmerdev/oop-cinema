31da34ca76f8d8aebd7c1a7ecc800b579f42f517
pedro-rs <pedro.rsr@outlook.com>
2022-07-10 22:24:34 -0300

    Added movie scheduling and playing.

--- content/generator.py
+++ content/generator.py
@@ -1,13 +1,28 @@
 # Gera conteudo para os diferentes filmes a serem colocados em cartaz,
 # como arquivo de video MP4, descricao, etc.
 # Sera usado como conexao com o módulo do :lib:‘manager’.
+from typing import List
+from interface.view import ViewFactory
 from movie import Movie, ScheduledMovie, CurrentMovie
 
 
 class MovieScheduler:
-    pass
+    def __init__(self):
+        self.scheduled_movies: List[Movie] = list()
+
+    def add(self, name: str, filename:str, duration: int, director: str, age_restricted: bool, start_time: int):
+        new_movie = ScheduledMovie(name, filename, duration, director, age_restricted, start_time)
+        self.scheduled_movies.append(new_movie)
+
+    def remove(self, name: str):
+        self.scheduled_movies = [movie for movie in self.scheduled_movies if movie.name != name]
 
 
 class MoviePlayer:
-    def __init__(self, movie):
-        pass
+    def __init__(self, movie: Movie, cinema: ViewFactory):
+        self.movie = movie
+        self.cinema = cinema
+
+    def play(self):
+        video = self.cinema.play_video(self.movie.filename, "../films/")
+        self.cinema.show(video)
--- content/movie.py
+++ content/movie.py
@@ -1,13 +1,16 @@
 class Movie:
-    def __init__(self):
-        pass
+    def __init__(self, name: str, filename:str, duration: int, director: str, age_restricted: bool, start_time: int):
+        self.name = name
+        self.filename = filename
+        self.duration = duration
+        self.director = director
+        self.age_restricted = age_restricted
+        self.start_time = start_time
 
 
 class ScheduledMovie(Movie):
-    def __init__(self):
-        super().__init__()
-        pass
-
+    def __init__(self, name: str, filename:str, duration: int, director: str, age_restricted: bool, start_time: int):
+        super().__init__(name, filename, duration, director, age_restricted, start_time)
 
 class CurrentMovie(Movie):
     pass
