e8499726d7bbd2fdd041b69244a6ffc6e732a73f
pedro-rs <pedro.rsr@outlook.com>
2022-07-11 15:44:22 -0300

    Ratings now allow for a comment.

--- content/movie.py
+++ content/movie.py
@@ -11,12 +11,28 @@ class Movie:
         self.rating = None
         self.__ratings = []
 
-    def add_rating(self, rate: int) -> Optional[float]:
+    def add_rating(self, rate: int, comment: Optional[str] = None) -> Optional[float]:
         if rate < 0 or rate > 5:
             raise ValueError("Rate should be in range [0, 5].")
 
-        self.__ratings.append(rate)
-        self.rating = sum(self.__ratings) / len(self.__ratings)
+        # Create a new rating
+        self.__ratings.append(Rating(rate, comment))
+        rates = [o.nome for o in self.__ratings]
+
+        # Compute new rates' mean
+        self.rating = sum(rates) / len(rates)
+
+
+class Rating:
+    def __init__(self, rate: int, comment: Optional[str] = None):
+        self.rate = rate
+        self.comment = comment
+
+    def __str__(self) -> str:
+        if self.comment is None:
+            return f"({self.rate}.0) '{self.comment}'"
+
+        return f"({self.rate}.0)"
         
 
 class ScheduledMovie(Movie):
