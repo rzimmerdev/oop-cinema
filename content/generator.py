5ba75213fbe3fdcdeaf40c15ebfdf466906df243
pedro-rs <pedro.rsr@outlook.com>
2022-07-11 14:09:40 -0300

    Added rating system for movies.

--- content/generator.py
+++ content/generator.py
@@ -1,7 +1,7 @@
 # Gera conteudo para os diferentes filmes a serem colocados em cartaz,
 # como arquivo de video MP4, descricao, etc.
 # Sera usado como conexao com o módulo do :lib:‘manager’.
-from typing import List
+from typing import List, Optional
 from interface.view import ViewFactory
 from movie import Movie, ScheduledMovie, CurrentMovie
 
@@ -10,12 +10,20 @@ class MovieScheduler:
     def __init__(self):
         self.scheduled_movies: List[Movie] = list()
 
-    def add(self, name: str, filename:str, duration: int, director: str, age_restricted: bool, start_time: int):
+    def add(self, name: str, filename:str, duration: int, director: str, age_restricted: bool, start_time: int) -> None:
         new_movie = ScheduledMovie(name, filename, duration, director, age_restricted, start_time)
         self.scheduled_movies.append(new_movie)
 
-    def remove(self, name: str):
+    def remove(self, name: str) -> None:
         self.scheduled_movies = [movie for movie in self.scheduled_movies if movie.name != name]
+
+    def get(self, name: str) -> Optional[Movie]:
+        for movie in self.scheduled_movies:
+            if movie.name == name:
+                return movie
+        
+        return None
+
 
 
 class MoviePlayer:
--- content/movie.py
+++ content/movie.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 class Movie:
     def __init__(self, name: str, filename:str, duration: int, director: str, age_restricted: bool, start_time: int):
         self.name = name
@@ -6,7 +8,16 @@ class Movie:
         self.director = director
         self.age_restricted = age_restricted
         self.start_time = start_time
+        self.rating = None
+        self.__ratings = []
 
+    def add_rating(self, rate: int) -> Optional[float]:
+        if rate < 0 or rate > 5:
+            raise ValueError("Rate should be in range [0, 5].")
+
+        self.__ratings.append(rate)
+        self.rating = sum(self.__ratings) / len(self.__ratings)
+        
 
 class ScheduledMovie(Movie):
     def __init__(self, name: str, filename:str, duration: int, director: str, age_restricted: bool, start_time: int):
