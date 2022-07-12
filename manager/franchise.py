aeb05b8130a51f31ebaf548f01ba4bb5db90cf78
JhonathanOliveira <jhonathanoliveiraalves2020@gmail.com>
2022-07-11 23:26:11 -0300

    updated cashier

--- manager/franchise.py
+++ manager/franchise.py
@@ -1,11 +1,11 @@
 # Classe para instanciar diversos tipos de franquias
+# franquia guarda os filmes, o filme atual e os ingressos
 
 class FranchiseFactory:
     pass
 
 
-class PrivateFranchise(FranchiseFactory):
-    #guarda os filmes, o filme atual e os ingressos
+class PrivateFranchise(FranchiseFactory): 
     pass
 
 
--- manager/manager.py
+++ manager/manager.py
@@ -1,7 +1,10 @@
 # Gerencia a compra e venda de ingressos dentro das diversas franquias
 
+from movie import Movie
+from random import randint
+
 class Ticket:
-    def __init__(self, identifier, barcode=None):
+    def __init__(self, identifier, barcode):
         self.identifier = identifier
         self.barcode = barcode
 
@@ -10,8 +13,10 @@ class Cashier:
     def __init__(self):
         self.sold_tickets = list()
 
-    def sell(self):
-        pass
+    def sell(self, card_number: int, cardholder_name: str, cvc: int, buyers_name: str, buyers_id: int):
+        self.sold_tickets.append(Ticket(len(self.sold_tickets), randint(0,100000)))
 
     def validate(self, ticket):
-        pass
+        if ticket in self.sold_tickets :
+            return True
+        return False
