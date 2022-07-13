57da2d451b3d594b44a47d1d51d112043dd143c9
JhonathanOliveira <jhonathanoliveiraalves2020@gmail.com>
2022-07-12 21:30:48 -0300

    Added franchiseFactory methods

--- manager/manager.py
+++ manager/manager.py
@@ -1,22 +1,35 @@
 # Gerencia a compra e venda de ingressos dentro das diversas franquias
 
-from movie import Movie
-from random import randint
+from xmlrpc.client import Boolean
+from content.movie import Movie
 
 class Ticket:
-    def __init__(self, identifier, barcode):
+    def __init__(self, identifier: int, movie: Movie):
+        self.movie = movie
         self.identifier = identifier
-        self.barcode = barcode
+
+    def get_movie_from_ticket(self) -> Movie:
+        return self.movie
 
 
 class Cashier:
     def __init__(self):
+        self.i = 0
+        self.money_amount = 0.0
         self.sold_tickets = list()
 
-    def sell(self, card_number: int, cardholder_name: str, cvc: int, buyers_name: str, buyers_id: int):
-        self.sold_tickets.append(Ticket(len(self.sold_tickets), randint(0,100000)))
+    def sell(self, price: float, movie: Movie) -> Ticket:
+        ticket = Ticket(self.i, movie)
+        
+        self.money_amount += price
+        self.sold_tickets.append(ticket)
+        self.i += 1
 
-    def validate(self, ticket):
+        return ticket
+
+    def validate(self, ticket) -> Boolean:
+        '''valida um ticket e remove ele da lista de tickets'''
         if ticket in self.sold_tickets :
+            self.sold_tickets.remove(ticket)
             return True
         return False
