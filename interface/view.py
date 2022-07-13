0551e821463d3d49eee5ad5b4dbc2842c03be57c
rzimmerdev <rzimmerdev@gmail.com>
2022-07-13 02:28:17 -0300

    Scrollable movies

--- interface/view.py
+++ interface/view.py
@@ -16,14 +16,14 @@ class ViewFactory:
     def __init__(self, franchise: str = None):
         self.franchise = franchise
         self.root = tk.Tk()
+        style = ttk.Style(self.root)
+        style.theme_use('alt')
         self.root.geometry("1840x1080")
         self.root.resizable(False, False)
         self.canvas = None
 
     def show(self):
         self.root.columnconfigure(0, weight=1)
-        self.root.columnconfigure(1, weight=2)
-        self.root.columnconfigure(2, weight=1)
         self.root.title(self.franchise)
         self.root.mainloop()
 
@@ -33,44 +33,54 @@ class ViewFactory:
         movie_player.show_tools(screen, file, self.root)
         movie_player.open(file)
         movie_player.play()
-        screen.grid(sticky="W", row=0, column=1)
+        screen.grid(row=0, column=1, sticky=tk.W)
 
     def movies_to_poster(self, movies: list[Movie]) -> tk.Frame:
-        posters = tk.Frame(self.root)
-        scroll = tk.Scrollbar(posters, orient='vertical')
-        scroll.pack(side=tk.RIGHT, fill=tk.Y)
+        posters = tk.Frame(self.root, height=300, width=100)
+        posters_canvas = tk.Canvas(posters)
+        posters_canvas.pack(side=tk.RIGHT, fill=tk.BOTH, expand=1)
 
-        posters_list = tk.Canvas(posters, yscrollcommand=scroll.set)
-        for movie in movies:
-            movie_frame = tk.Frame(posters)
-            name = tk.Label(movie_frame, text=movie.name)
-            description = tk.Label(movie_frame, text=movie.description, wraplength=256)
-            time = tk.Label(movie_frame, text=str(movie.duration // 60) + ":" + "{:02d}".format(movie.duration % 60))
+        scroll = tk.Scrollbar(posters, orient="vertical", command=posters_canvas.yview)
+        scroll.pack(side=tk.LEFT, fill=tk.Y)
+
+        posters_canvas.configure(yscrollcommand=scroll.set)
+        posters_canvas.bind('<Configure>', lambda e: posters_canvas.configure(scrollregion=posters_canvas.bbox("all")))
+
+        scroll_frame = tk.Frame(posters_canvas)
+        posters_canvas.create_window((0, 0), window=scroll_frame, anchor="nw")
+        for row, movie in enumerate(movies):
+            movie_frame = tk.Frame(scroll_frame)
             thumbnail = ImageTk.PhotoImage(Image.open("posters/" + movie.thumbnail).resize((256, 256)))
-
             image_label = tk.Label(movie_frame, image=thumbnail)
             image_label.image = thumbnail
+            image_label.grid(row=0, column=0)
+            name = tk.Label(movie_frame, text=movie.name)
+            name.grid(row=1, column=0)
 
-            name.pack()
-            description.pack()
-            time.pack()
-            image_label.pack()
-
-            movie_frame.pack(pady=(0, 40))
-        scroll.config(command=posters_list.yview)
+            # description = tk.Label(movie_frame, text=movie.description, wraplength=256)
+            # time = tk.Label(movie_frame, text=str(movie.duration // 60) + ":" + "{:02d}".format(movie.duration % 60))
+            # thumbnail = ImageTk.PhotoImage(Image.open("posters/" + movie.thumbnail).resize((256, 256)))
+            #
+            # image_label = tk.Label(movie_frame, image=thumbnail)
+            # image_label.image = thumbnail
+            #
+            # name.grid()
+            # description.grid()
+            # time.grid()
+            # image_label.grid()
+            #
+            movie_frame.grid(row=row, column=0, pady=(0, 40))
         return posters
 
     def show_movies(self, movies: list[Movie] = None):
         if not movies:
             movies = list()
-        poster_list = self.movies_to_poster(movies)
-        poster_list.grid(sticky="W", row=0, column=0, pady=50, padx=70)
+        posters = self.movies_to_poster(movies)
+        posters.grid(row=0, column=0, sticky=tk.W)
 
 
 def main():
     cinema1 = ViewFactory(franchise="CinePlex")
-    cinema1.play_src("water.mkv", "films/")
-
     movies = [Movie("Minions: Rise of Gru", "water.mkv",
                     "In the 1970s, young Gru tries to join a group of supervillains, "
                     "called the Vicious 6 after they oust their leader",
@@ -87,7 +97,7 @@ def main():
                     "called the Vicious 6 after they oust their leader",
                     1071, 141, thumbnail="minions.jpg", age_restricted=False)
               ]
-
+    cinema1.play_src(movies[0].filename, "films/")
     cinema1.show_movies(movies)
     cinema1.show()
 
